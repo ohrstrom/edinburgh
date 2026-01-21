7145e5ebb9f163966a21cf1bc266db8382140c4a
Jonas Ohrstrom <ohrstrom@gmail.com>
2026-01-21 14:12:16 +0100

    ci: add build

--- demo/docker/start.sh
+++ demo/docker/start.sh
@@ -2,6 +2,6 @@ nginx -c $(pwd)/nginx.conf &
 
 edinburgh-frame-forwarder &
 
-edinburgh-ensemble-directory --scan edi-ch.digris.net:8851-8865 &
+edinburgh-ensemble-directory --scan edi-ch.digris.net:8851-8866 --scan edi-fr.digris.net:8851-8852 --scan edi-fr.digris.net:8854-8880 --scan edi-uk.digris.net:8851-8859 &
 
 wait
